8da9fe449b896ebdc7e5fa108e728c5efccee39a
MuriloAltoe <muriloaleme17@gmail.com>
2022-11-01 21:21:36 -0300

    'first-commit'

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <index-vue></index-vue>
+</template>
+
+<script>
+
+import indexVue from '../pages/index.vue';
+
+export default {
+
+  components: {
+    indexVue,
+  },
+
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
