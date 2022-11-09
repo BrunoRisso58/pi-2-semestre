7645fd3d3525680eed499183ea92fe1ff3786ec9
Bruno Risso <brunorisso58@gmail.com>
2022-11-09 12:20:36 -0300

    Classes Login e Database

--- backend/classes/Login.php
+++ backend/classes/Login.php
@@ -2,7 +2,35 @@
 
 class Login {
 
-    
+    private $email;
+    private $password;
+
+    function login() {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            session_start();
+            if($_POST['username'] == $this->email and $_POST['password'] == $this->password) {
+                $_SESSION['loggedin'] = TRUE;
+                $_SESSION["username"] = $this->email;
+            } else {
+                $_SESSION['loggedin'] = FALSE;
+            }
+        }
+    }
+
+    function logout() {
+        session_start();
+        $_SESSION = array();
+        session_destroy();
+        header("location: index.php");
+        exit;
+    }
+
+    function verifyLogin() {
+        session_start();
+        if ($_SESSION['loggedin'] == FALSE) {
+            header("location: index.php");
+        }
+    }
 
 }
 
