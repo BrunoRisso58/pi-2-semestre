f9100e21e3d63c12958ff92a2937b67b411309fb
MuriloAltoe <muriloaleme17@gmail.com>
2022-11-15 15:28:40 -0300

    defaultPages-add

---
+++ pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div v-if="$vuetify.breakpoint.name !== 'xs'">
+    <header-vue />
+</div>
+
+<div v-else>    
+    <header-mobile-vue />
+</div>
+
+</template>
+
+<script>
+import header from '../components/header/header.vue'
+import HeaderMobile from '../components/header/headerMobile.vue'
+export default {
+  components: { header, HeaderMobile },
+
+}
+</script>
+
+<style>
+
+</style>
