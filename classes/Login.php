3a32ab991a6abcb97fe3985b9168af7bf5c127fb
Bruno Risso <brunorisso58@gmail.com>
2022-12-12 11:23:57 -0300

    Alterando função de login

--- classes/Login.php
+++ classes/Login.php
@@ -5,12 +5,12 @@ class Login {
     private $email;
     private $password;
 
-    function login() {
+    function login($email, $password) {
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             session_start();
-            if($_POST['username'] == $this->email and $_POST['password'] == $this->password) {
+            if($email == $this->email and $password == $this->password) {
                 $_SESSION['loggedin'] = TRUE;
-                $_SESSION['username'] = $this->email;
+                $_SESSION['email'] = $this->email;
             } else {
                 $_SESSION['loggedin'] = FALSE;
             }
